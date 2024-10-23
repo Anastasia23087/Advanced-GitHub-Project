e84245c2f40bae35e2c1d65bbffbf31f8538c4fb
Иванов Иван Иванович <ivanov.email@example.com>
2024-10-23 14:06:52 +0300

    Integrated helper functions as submodule

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import {greeting} from './helper-functions/greeting.js';
 function helloWorld() {
     console.log("Welcome to advanced GitHub project!!!");
 }
 helloWorld();
+console.log(greeting("GitHub User"));
